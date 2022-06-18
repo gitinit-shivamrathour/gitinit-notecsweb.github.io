07fa9694f95b60b4bc7ecea141ac26ef8517a434
Shivam Rathour <shivampublic2001@gmail.com>
2022-06-18 22:55:48 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var typed = new Typed(".typing", {
-        strings: ["Cse. Notes.com", "Cse. Notes.in", "Cse. Notes.com.in", "Cse. Notes.tech"],
-        typeSpeed: 60,
-        backSpeed: 40,
-        loop: true
-    });
-});
