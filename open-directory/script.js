1bb0e9cc7648c0d4d4893cf607f8d59991bab4fb
Shivam Rathour <shivampublic2001@gmail.com>
2022-06-18 22:53:04 +0530

    Add files via upload

---
+++ open-directory/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Cse. Notes.com", "Cse. Notes.in", "Cse. Notes.com.in", "Cse. Notes.tech"],
+        typeSpeed: 60,
+        backSpeed: 40,
+        loop: true
+    });
+});
